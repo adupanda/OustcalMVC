a50fd9940154ee8bad37d15e30e2df66639b0ffa
adupanda <aditya.dharmarajan@gmail.com>
2024-04-23 22:19:53 +0530

    Tank UI

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -20,13 +20,31 @@ public class TankSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CreateTank();
+        
     }
 
-    private void CreateTank()
+    public void CreateTank(TankTypes tankType)
     {
-        TankModel tankModel = new TankModel(tankList[2].movementSpeed, tankList[2].rotationSpeed, tankList[2].tankType, tankList[2].color);
-        TankController tankController = new TankController(tankModel, tankView);
+
+        switch (tankType)
+        {
+            case TankTypes.GreenTank:
+                TankModel greenTankModel = new TankModel(tankList[0].movementSpeed, tankList[0].rotationSpeed, tankList[0].tankType, tankList[0].color);
+                TankController greenTankController = new TankController(greenTankModel, tankView);
+                break;
+            case TankTypes.BlueTank:
+                TankModel blueTankModel = new TankModel(tankList[1].movementSpeed, tankList[1].rotationSpeed, tankList[1].tankType, tankList[1].color);
+                TankController blueTankController = new TankController(blueTankModel, tankView);
+                break;
+            case TankTypes.RedTank:
+                TankModel redTankModel = new TankModel(tankList[2].movementSpeed, tankList[2].rotationSpeed, tankList[2].tankType, tankList[2].color);
+                TankController redTankController = new TankController(redTankModel, tankView);
+                break;
+
+
+        }
+
+        
     }
     
 }

1dd75e7b27c772a7a7c1e3c7b1d023ef2e0e8831
adupanda <aditya.dharmarajan@gmail.com>
2024-04-23 21:15:42 +0530

    Added tank

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class TankSpawner : MonoBehaviour
 {
-
+    public GameObject tankPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(tankPrefab,transform.position,Quaternion.identity);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
